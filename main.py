eb48cb2ba2d4417e894bad8e63afd51f125f3140
Rich Yeates <richardyeates37@gmail.com>
2025-06-24 21:06:53 -0500

    Completed the Boot.dev project

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from stats import get_word_count
-
+from stats import get_word_count, get_character_count, sort_char_by_count as so
+import sys
 def get_book_text(filePath):
     with open(filePath) as f:
         return f.read()
 
 
 def main():
-    frankensteinPath = "books/frankenstein.txt"
-    bookText = get_book_text(frankensteinPath)
+    bookPath = sys.argv[1] if len(sys.argv) > 1 else sys.exit("Usage: python3 main.py <path_to_book>")
+    bookText = get_book_text(bookPath)
     wordCount = get_word_count(bookText)
-    print(f"{wordCount} words found in the document")
+    charCount = get_character_count(bookText)
+    charCount = so(charCount)
+    print_report(bookPath, wordCount, charCount)
+    
+
+def print_report(path, wordCount, charCount):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at: {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {wordCount} total words")
+    print("---------- Character Count -------")
+    for char in charCount:
+        print(f"{char['char']}: {char['num']} times")
+    print("=================================")
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,21 @@
 def get_word_count(text):
     return len(text.split())
+
+def get_character_count(text):
+    charDict = {}
+    for char in text.lower():
+        if char in charDict:
+            charDict[char] += 1
+        else:
+            charDict[char] = 1
+    return charDict
+
+def sort_on(d):
+    return d["num"]
+
+def sort_char_by_count(charDict):
+    sortedList =[]
+    for char in charDict:
+        sortedList.append({"char": char, "num": charDict[char]})
+    sortedList.sort(key=sort_on, reverse=True)
+    return sortedList
