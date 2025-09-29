b32cd4e0c24ed14339a0aa0b914a7a2649b101d4
charge95 <richardyeates37@gmail.com>
2025-09-28 21:45:13 -0500

    Read frankenstien file and print to stout.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+def get_book_text(path):
+    with open(path) as book:
+        book_contents = book.read()
+        return book_contents
+    
+main()
